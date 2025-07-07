8ba1e69fa8e740f5d691b637740028f7518dd916
Inder Grewal <inderdeepg8@gmail.com>
2025-07-06 20:11:48 -0700

    Created Components for each section of the CV, including form to collect and store data in States, to convert into HTML elements.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import General from '/src/components/General.jsx';
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <General />
+    </>
+  );
+}
---
+++ src/components/General.jsx
@@ -0,0 +1,152 @@
+import { useState } from "react";
+import "../App.css";
+
+export default function General() {
+  const [edit, setEdit] = useState(true);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+    phoneNumber: "",
+    Address: "",
+    LinkedIn: "",
+    GitHub: "",
+    personalWebsite: "",
+  });
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setEdit(false);
+  }
+
+  if (edit) {
+    return (
+        <form onSubmit={handleSubmit}>
+        <div className="input_field_links name">
+            <Name nameData={formData} onChange={handleInputChange} />
+        </div>
+
+        <div className="input_field_links info">
+            <Info infoData={formData} onChange={handleInputChange} />
+        </div>
+
+        <div className="input_field_links link">
+            <Links linkData={formData} onChange={handleInputChange} />
+        </div>
+
+        <button className="submit_general" type="submit" onClick={handleSubmit}>Submit</button>
+        </form>
+    );
+  } else {
+    const fullName = (formData.firstName + ' ' + formData.lastName).toUpperCase();
+    return (
+        <>
+            <h1>{fullName}</h1>
+            <div>test</div>
+        </>
+    );
+  }
+}
+
+function Name({ nameData, onChange }) {
+  return (
+    <>
+      <div className="input_field">
+        <label>First Name</label>
+        <input
+          type="text"
+          name="firstName"
+          value={nameData.firstName}
+          onChange={onChange}
+        />
+      </div>
+      <div className="input_field">
+        <label>Last Name</label>
+        <input
+          type="text"
+          name="lastName"
+          value={nameData.lastName}
+          onChange={onChange}
+        />
+      </div>
+    </>
+  );
+}
+
+function Info({ infoData, onChange }) {
+  return (
+    <>
+      <div className="input_field">
+        <label>Email Address</label>
+        <input
+          type="email"
+          name="emailAddress"
+          value={infoData.emailAddress}
+          onChange={onChange}
+        />
+      </div>
+
+      <div className="input_field">
+        <label>Phone Number</label>
+        <input
+          type="tel"
+          name="phoneNumber"
+          value={infoData.phoneNumber}
+          onChange={onChange}
+        />
+      </div>
+
+      <div className="input_field">
+        <label>Address</label>
+        <input
+          type="text"
+          name="Address"
+          value={infoData.Address}
+          onChange={onChange}
+        />
+      </div>
+    </>
+  );
+}
+
+function Links({ linkData, onChange }) {
+  return (
+    <>
+      <div className="input_field">
+        <label>LinkedIn</label>
+        <input
+          type="text"
+          name="LinkedIn"
+          value={linkData.LinkedIn}
+          onChange={onChange}
+        />
+      </div>
+      <div className="input_field">
+        <label>GitHub</label>
+        <input
+          type="text"
+          name="GitHub"
+          value={linkData.GitHub}
+          onChange={onChange}
+        />
+      </div>
+      <div className="input_field">
+        <label>Personal Website</label>
+        <input
+          type="text"
+          name="personalWebsite"
+          value={linkData.personalWebsite}
+          onChange={onChange}
+        />
+      </div>
+    </>
+  );
+}
